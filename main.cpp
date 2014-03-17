f49dc634b0c5ac3c75eb6b77cf0cd0bcb0a198e8
Brian Jackson <axnjaxn@axnjaxn.com>
2014-03-17 14:09:51 -0400

    Added depth-of-field and started working on a fix for the grid.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #define TITLE "ReiTrei5"
 //#define NO_GRID
 
-#include <ctime>
 #include "ray5screen.h"
 #include "ray5parser.h"
 #include "ray5shapes.h"
+#include <SDL/SDL.h>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 
 Vect4 traceRay(const Ray5Scene& scene, const Vect4& O, const Vect4& D, int nrecurse = 0) {
   Vect4 color;
@@ -58,7 +60,7 @@ Vect4 traceRay(const Ray5Scene& scene, const Vect4& O, const Vect4& D, int nrecu
 
 #include <csignal>
 inline void segFault(int param) {
-  printf("Segmentation fault.");
+  printf("Segmentation fault.\n");
   exit(0);
 }
 
@@ -81,12 +83,27 @@ int stripExtension(char* str) {
 
 inline Uint8 toByte(Real r) {return (r > 1.0)? 255 : (Uint8)(255 * r);}
 
-int render(Ray5Scene& scene, Ray5Screen& r_screen) {
+void redraw(const Ray5Screen& r_screen) {
+  SDL_Surface* screen = SDL_GetVideoSurface();
+  if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
+
+  Uint32* bufp = (Uint32*)screen->pixels;
+  Vect4 color;
+  for (int r = 0; r < r_screen.height(); r++) {
+    for (int c = 0; c < r_screen.width(); c++) {
+      color = r_screen.getColor(r, c);
+      bufp[r * screen->w + c] = SDL_MapRGB(screen->format, toByte(color[0]), toByte(color[1]), toByte(color[2]));
+    }
+  }
+  
+  if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
+  SDL_Flip(screen);
+}
+
+void render(Ray5Scene& scene, Ray5Screen& r_screen) {
   char titlebuf[200];
   sprintf(titlebuf, "%s",TITLE);
   SDL_WM_SetCaption(titlebuf, NULL);
-
-  Uint32 started = SDL_GetTicks();
 
   SDL_Surface* screen = SDL_GetVideoSurface();
   if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
@@ -114,19 +131,13 @@ int render(Ray5Scene& scene, Ray5Screen& r_screen) {
   }
 
   if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
-
-  printf("Tracing complete:\n");
-  printf("\tTotal time elapsed: %.3fs\n", 0.001 * (SDL_GetTicks() - started));
-  fflush(0);
-
-  SDL_SaveBMP(SDL_GetVideoSurface(), "out.bmp");
-
-  return 0;
 }
 
+inline float uniform() {return (rand() & 0x7FFF) / 32768.0;}
+
 void printUsage() {
-  printf("ReiTrei4 by Brian Jackson\n");
-  printf("Usage: ReiTrei4 [options] [scene.ray]\n");
+  printf("ReiTrei5 by Brian Jackson\n");
+  printf("Usage: ReiTrei5 [options] [scene.ray]\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -147,11 +158,19 @@ int main(int argc, char* argv[]) {
   Ray5Scene& scene = *Ray5Scene::getInstance();
   Ray5Screen r_screen;
   int w = 300, h = 300;
-  float aspect;
+  int nrenders = 1;
+  float dof_range = 0.0;
   for (int i = 1; i < argc - 1; i++) {
     if (!strcmp(argv[i], "--size")) {
       sscanf(argv[++i], "%d", &w);
       sscanf(argv[++i], "%d", &h);
+    }
+    else if (!strcmp(argv[i], "--renders")) {
+      sscanf(argv[++i], "%d", &nrenders);
+    }
+    else if (!strcmp(argv[i], "--dof_degrees")) {
+      sscanf(argv[++i], "%f", &dof_range);
+      dof_range *= PI / 180.0;
     }
     else {
       printf("Unrecognized token \"%s\".\n", argv[i]);
@@ -165,8 +184,7 @@ int main(int argc, char* argv[]) {
   /*
    * Set up scene
    */
-  aspect = (float)w / h;
-  scene.camera.setScreen(w, h, aspect, 1, PI / 2);
+  scene.camera.setScreen(w, h, (float)w / h, 1, PI / 2);
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) exit(1);
   atexit(SDL_Quit);
@@ -174,7 +192,35 @@ int main(int argc, char* argv[]) {
 
   r_screen.setDimensions(screen->w, screen->h);
 
-  render(scene, r_screen);
+  Uint32 started = SDL_GetTicks();
+  
+  std::vector<Ray5Screen> screens;
+
+  float rmag, rth, rx, ry;
+  for (int i = 0; i < nrenders; i++) {    
+    if (dof_range > 0.0) {
+      rmag = uniform() * dof_range; 
+      rth = uniform() * 2 * PI;
+      rx = rmag * cos(rth);
+      ry = rmag * sin(rth);
+    }
+    else {rx = ry = 0.0;}
+    scene.camera.xrotate(rx);
+    scene.camera.yrotate(ry);
+    render(scene, r_screen);
+    scene.camera.yrotate(-ry);
+    scene.camera.xrotate(-rx);
+    screens.push_back(r_screen);
+  }
+
+  r_screen = Ray5Screen(screens);
+  redraw(r_screen);
+
+  printf("Tracing complete:\n");
+  printf("\tTotal time elapsed: %.3fs\n", 0.001 * (SDL_GetTicks() - started));
+  fflush(0);
+
+  SDL_SaveBMP(SDL_GetVideoSurface(), "out.bmp");
 
   SDL_Event event;
   bool exitflag = 0;
